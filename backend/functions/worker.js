79f19c63c381378a9ea48ee76f16a160325d8258
dushmilan <dushmilan03@gmail.com>
2025-12-24 22:44:35 +0530

    Made wrangler changes

--- backend/functions/worker.js
+++ backend/functions/worker.js
@@ -8,7 +8,7 @@ module.exports = {
     process.env.SUPABASE_URL = env.SUPABASE_URL;
     process.env.SUPABASE_KEY = env.SUPABASE_KEY;
     process.env.CF_WORKERS = 'true';
-    process.env.NODE_ENV = 'production';
+    // process.env.NODE_ENV = 'production';
 
     const handler = serverless(serverApp);
     return handler(request, ctx);
